9af4ee0b581ddac1402c7789f780289f5141829e
chriz <shambler.qc@gmail.com>
2012-12-14 07:58:58 -0600

    disk map structure defs

--- render/dmap.h
+++ render/dmap.h
@@ -3,6 +3,12 @@
 
 /* map structures as formatted on disk */
 
+struct dplane_s
+{
+	float normal[3];
+	float dist;
+};
+
 struct dvertex_s
 {
 	float x, y, z;
@@ -10,8 +16,59 @@ struct dvertex_s
 
 struct dedge_s
 {
-	unsigned int v1, v2;
+	unsigned short v1, v2;
 };
+
+struct dsector_s
+{
+	unsigned short lightlevel;
+	unsigned short lightstyle;
+	unsigned short contents;
+};
+
+struct dface_s
+{
+	unsigned short sector;
+	//TODO: texture, has transparent pixels
+	//TODO: vecs
+};
+
+struct dsurface_s
+{
+	unsigned int first_surfedge;
+	short num_surfedges;
+
+	unsigned short plane;
+	short side;
+
+	unsigned short face; /* -1 for portals */
+};
+
+struct dleaf_s
+{
+	short mins[3];
+	short maxs[3];
+
+	unsigned short first_surface;
+	short num_surface;
+};
+
+struct dnode_s
+{
+	short mins[3];
+	short maxs[3];
+
+	/* all on-node surfaces must reference the node's plane */
+	unsigned short first_surface;
+	short num_surface;
+
+	unsigned short plane;
+
+	unsigned short front, back; /* high  bit if a leaf */
+};
+
+//...
+//...
 
 struct dvertex2d_s
 {
