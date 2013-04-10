005626928382b9116c6fb2278646a1f3526791c4
chriz <shambler.qc@gmail.com>
2013-04-09 20:05:19 -0500

    try to auto-detect the map name if not given

--- utils/mybsp.py
+++ utils/mybsp.py
@@ -10,12 +10,22 @@ import runbsp
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print "usage: %s <wad> <mapname>" % sys.argv[0]
+    if len(sys.argv) < 2:
+        print "usage: %s <wad> [<mapname>]" % sys.argv[0]
         sys.exit(0)
     w = wad.Wad(sys.argv[1])
 
-    for mapname in sys.argv[2:]:
+    if len(sys.argv) > 2:
+        mapnames = sys.argv[2:]
+    else:
+        # no map name given; let user be lazy and auto-detect it
+        #TODO: could be done more intelligently
+        if len(w.lumps) == 11 and w.lumps[0].size == 0:
+            mapnames = [w.lumps[0].name]
+        else:
+            raise Exception("unable to auto-detect map")
+
+    for mapname in mapnames:
         inmap.load(w, mapname)
 
         runbsp.runBSP()
